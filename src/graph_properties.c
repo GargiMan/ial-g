b36acf009d47afbdd1ce0c7c028e1688c61173eb
Jindřich Šíma <67415662+JindrichSima@users.noreply.github.com>
2022-11-04 15:41:16 +0100

    graph_is_connected_graph()

--- src/graph_properties.c
+++ src/graph_properties.c
@@ -13,8 +13,54 @@
 #include "../include/graph_properties.h"
 #include "../include/graph.h"
 
-int graph_is_connected_graph() // 0 - non, 1 - weak, 2 - strong
+/**
+ * @brief deep first search function
+ *  It goes through all neighbors of the inserted nodes,
+ *  if any of them has already been checked, it skips,
+ *  otherwise the selected one is called recursively until all nodes are checked.
+ * @param n Input node for DFS
+ */
+void DFS(node_t *n)
 {
+	n->visited = 1;
+	int neb = n->edge_count;
+	for (int i = 0; i < neb; i++)
+	{
+		if (n->edge_nodes[i]->visited != 1)
+		{
+			DFS(n->edge_nodes[i]);
+		}
+	}
+}
+
+/**
+ * @brief deep-first search function to determine if the graph is continuous.
+ * @author Jindřich Šíma (xsimaj04)
+ * @return true Graph is connected.
+ * @return false Graph is not connected.
+ */
+bool graph_is_connected_graph()
+{
+	if (graph_is_isolated()) // if the graph is isolated it may not continue.
+	{
+		return false;
+	}
+	int node_count = graph_get_node_count();
+
+	DFS(graph->nodes[0]); // deep-first search
+
+	/**
+	 * all nodes are passed through,
+	 * if some nodes have not been visited it is a disconnected graph.
+	 */
+	for (int k = 0; k < node_count; k++)
+	{
+		if (graph->nodes[k]->visited == 0)
+		{
+			return false;
+		}
+	}
+	return true;
 }
 
 /**
@@ -80,15 +126,27 @@ int graph_max_numb_degree()
 }
 
 /**
- * @brief analyze properties of a graph
+ * @brief analyze graph properties and list them.
  *
  */
 void graph_analyze_properties()
 {
-	printf("Node count: %d\n", graph_get_node_count());
-	printf("Edge count: %d\n", graph_get_edge_count());
-	printf("Maximum degree: %d\n", graph_max_numb_degree());
-	printf("Graph is an isolated: %s\n", graph_is_isolated() == true ? "yes" : "no");
-	printf("Graph is an complete: %s\n", graph_is_complete_graph() == true ? "yes" : "no");
+	printf("================================================\n");
+	printf("Node count:            %d\n", graph_get_node_count());
+	printf("Edge count:            %d\n", graph_get_edge_count());
+	printf("Maximum degree:        %d\n", graph_max_numb_degree());
+	if (graph_max_numb_degree() != 0)
+	{
+		printf("Graph is an isolated:  %s\n", graph_is_isolated() == true ? "yes" : "no");
+		printf("Graph is an complete:  %s\n", graph_is_complete_graph() == true ? "yes" : "no");
+		printf("Graph is an connected: %s\n", graph_is_connected_graph() == true ? "yes" : "no");
+	}
+	else
+	{
+		printf("Graph is an isolated:  EMPTY GRAPH\n");
+		printf("Graph is an complete:  EMPTY GRAPH\n");
+		printf("Graph is an connected: EMPTY GRAPH\n");
+	}
+	printf("================================================\n");
 	// call property functions
 }
