dd906ace0f8f681219093cdb44fd1d52798cd49b
Jindřich Šíma <67415662+JindrichSima@users.noreply.github.com>
2022-11-04 13:59:02 +0100

    Add files via upload

--- src/graph_properties.c
+++ src/graph_properties.c
@@ -13,14 +13,33 @@
 #include "../include/graph_properties.h"
 #include "../include/graph.h"
 
-bool graph_is_complete_graph()
+int graph_is_connected_graph() // 0 - non, 1 - weak, 2 - strong
 {
 }
 
 /**
+ * @brief Loops through all nodes, compares the number of vertices, compares with the completeness condition.
+ * @author Jindřich Šíma (xsimaj04)
+ * @return true Graph is complete.
+ * @return false Graph is not complete.
+ */
+bool graph_is_complete_graph()
+{
+	int node_count = graph_get_node_count();
+	int mustCount = (node_count * (node_count - 1)) / 4; // The number of edges in the graph must be N(N-1)/2
+
+	for (int k = 0; k < node_count; k++)
+	{
+		if (graph->nodes[k]->edge_count != mustCount)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+/**
  * @brief Loops through all nodes, compares each vertex count, compares and find an isolated vertex.
  * @author Jindřich Šíma (xsimaj04)
- * @warning If an error occurs, -1 is returned.
  * @return true if there is at least one isolated node.
  * @return false if there are no isolated nodes.
  */
@@ -68,8 +87,8 @@ void graph_analyze_properties()
 {
 	printf("Node count: %d\n", graph_get_node_count());
 	printf("Edge count: %d\n", graph_get_edge_count());
-
 	printf("Maximum degree: %d\n", graph_max_numb_degree());
 	printf("Graph is an isolated: %s\n", graph_is_isolated() == true ? "yes" : "no");
+	printf("Graph is an complete: %s\n", graph_is_complete_graph() == true ? "yes" : "no");
 	// call property functions
 }
