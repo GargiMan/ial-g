553f3933a96447de47890c1426a8bb6e6b031609
Jakub Pekárek <jakub.pek@seznam.cz>
2023-10-10 17:03:43 +0000

    Parser rework

--- src/parser.c
+++ src/parser.c
@@ -15,228 +15,78 @@ FILE *stream_s;
 
 int lines = 1;
 int columns = 1;
-
-/**
- * @brief Function parse node data to graph structure
- */
-void parse_node_data()
-{
-
-    bool list_start = false;
-    bool list_split = false;
-    bool list_end = false;
-
-    char node_name[MAX_NODE_NAME_LENGTH + 1] = "\0";
-
-    while (!list_end)
-    {
-
-        char last_char = fgetc(stream_s);
-
-        switch (last_char)
-        {
-
-        case -1:
-            error_exit(parserSyntaxError, "Graph is not finished, unexpected 'EOF'\n");
-            break;
-
-        case '\n':
-        case '\r':
-            if (strlen(node_name) != 0)
-            {
-                error_exit(parserSyntaxError, "Unexpected 'EOL' at position %i:%i\n", lines, columns);
-            }
-            lines++;
-            columns = 0;
-            break;
-
-        case '{':
-            if (list_start)
-            {
-                error_exit(parserSyntaxError, "Unexpected '%c' at position %i:%i\n", last_char, lines, columns);
-            }
-            list_start = true;
-            break;
-
-        case '}':
-            if (!list_start || list_split)
-            {
-                error_exit(parserSyntaxError, "Unexpected '%c' at position %i:%i\n", last_char, lines, columns);
-            }
-            list_end = true;
-            break;
-
-        case ',':
-            if (!list_start || strlen(node_name) == 0)
-            {
-                error_exit(parserSyntaxError, "Unexpected '%c' at position %i:%i\n", last_char, lines, columns);
-            }
-            list_split = true;
-            break;
-
-        default:
-            if (!list_start || !((last_char >= 'a' && last_char <= 'z') || (last_char >= 'A' && last_char <= 'Z') || (last_char >= '0' && last_char <= '9')))
-            {
-                error_exit(parserSyntaxError, "Unexpected '%c' at position %i:%i\n", last_char, lines, columns);
-            }
-
-            list_split = false;
-
-            if (strlen(node_name) == MAX_NODE_NAME_LENGTH)
-            {
-                error_exit(parserNodeNameLengthOverflowError, "Node name lenght overflow (max %i characters) at position %i:%i\n", MAX_NODE_NAME_LENGTH, lines, columns);
-            }
-            node_name[strlen(node_name)] = last_char;
-            break;
-        }
-
-        if ((last_char == ',' || last_char == '}') && strlen(node_name) != 0)
-        {
-            graph_create_node(node_name);
-            int node_name_len = (int)strlen(node_name);
-            for (int i = 0; i < node_name_len; i++)
-            {
-                node_name[i] = '\0';
-            }
-        }
-
-        columns++;
-    }
-
-    if (graph_get_node_count() == 0)
-    {
-        error_exit(parserNodeCountZeroError, "Graph node list is empty\n");
-    }
-}
+int line_digit_count = 0;
+int column_digit_count = 0;
 
 /**
  * @brief Function parse edge data to graph structure
  */
 void parse_edge_data()
-{
+{    
+    char last_char = fgetc(stream_s);
 
-    bool list_start = false;
-    bool list_split = true;
-    bool list_end = false;
-
-    bool edge_start = false;
-    bool edge_split = false;
-    bool edge_end = false;
-
-    char node_name[MAX_NODE_NAME_LENGTH + 1] = "\0";
-    char node2_name[MAX_NODE_NAME_LENGTH + 1] = "\0";
-
-    while (!list_end)
+    while (last_char != EOF)
     {
-
-        char last_char = fgetc(stream_s);
-
-        switch (last_char)
+        //Counting number of digits 
+        line_digit_count = 0;
+        int temp_lines = lines;
+        if (temp_lines != 0)
         {
+            while(temp_lines != 0)  
+            {  
+                temp_lines = temp_lines/10;  
+                line_digit_count++;  
+            } 
+        }
+        else 
+        {
+            line_digit_count = 1;
+        }
+        
+        //Creating name from line number
+        char node_name[line_digit_count + 1];
+        sprintf(node_name, "%d", lines);
+        graph_create_node(node_name);
 
-        case -1:
-            error_exit(parserSyntaxError, "Graph is not finished, unexpected 'EOF'\n");
-            break;
-
-        case '\n':
-        case '\r':
-            if (strlen(node_name) != 0 || strlen(node2_name) != 0)
+        for (int column_index = 1; column_index <= lines; column_index++)
+        {
+            while (last_char == ' ')
             {
-                error_exit(parserSyntaxError, "Unexpected 'EOL' at position %i:%i\n", lines, columns);
-            }
-            lines++;
-            columns = 0;
-            break;
-
-        case '{':
-            if (list_start)
-            {
-                error_exit(parserSyntaxError, "Unexpected '%c' at position %i:%i\n", last_char, lines, columns);
-            }
-            list_start = true;
-            break;
-
-        case '}':
-            if (!list_start || edge_start)
-            {
-                error_exit(parserSyntaxError, "Unexpected '%c' at position %i:%i\n", last_char, lines, columns);
-            }
-            list_end = true;
-            break;
-
-        case '(':
-            if (!list_start || edge_start || !list_split)
-            {
-                error_exit(parserSyntaxError, "Unexpected '%c' at position %i:%i\n", last_char, lines, columns);
-            }
-            edge_start = true;
-            list_split = false;
-            break;
-
-        case ')':
-            if (!list_start || !edge_start || !edge_split || strlen(node2_name) == 0)
-            {
-                error_exit(parserSyntaxError, "Unexpected '%c' at position %i:%i\n", last_char, lines, columns);
-            }
-            edge_end = true;
-            break;
-
-        case ',':
-            if (!list_start || edge_split || list_split || (edge_start && strlen(node_name) == 0))
-            {
-                error_exit(parserSyntaxError, "Unexpected '%c' at position %i:%i\n", last_char, lines, columns);
-            }
-            if (edge_start)
-            {
-                edge_split = true;
-            }
-            else
-            {
-                list_split = true;
-            }
-            break;
-
-        default:
-            if (!list_start || !edge_start || !((last_char >= 'a' && last_char <= 'z') || (last_char >= 'A' && last_char <= 'Z') || (last_char >= '0' && last_char <= '9')))
-            {
-                error_exit(parserSyntaxError, "Unexpected '%c' at position %i:%i\n", last_char, lines, columns);
+                last_char = fgetc(stream_s);
             }
 
-            if (strlen(node_name) == MAX_NODE_NAME_LENGTH || strlen(node2_name) == MAX_NODE_NAME_LENGTH)
+            if (!(last_char == '0' || last_char == '1'))
             {
-                error_exit(parserNodeNameLengthOverflowError, "Node name lenght overflow (max %i characters) at position %i:%i\n", MAX_NODE_NAME_LENGTH, lines, columns);
+                error_exit(parserSyntaxError, "Unexpected '%c' at position %i:%i\n", last_char, lines, column_index);
             }
 
-            if (!edge_split)
+            if (last_char == '1')
             {
-                node_name[strlen(node_name)] = last_char;
+                column_digit_count = 0;
+                int temp_column = column_index;
+                if (temp_column != 0)
+                {
+                    while(temp_column != 0)  
+                    {  
+                        temp_column = temp_column/10;  
+                        column_digit_count++;  
+                    }
+                }
+                else 
+                {
+                    column_digit_count = 1;
+                }
+                // Creating name from column index
+                char node_name2[column_digit_count + 1];
+                sprintf(node_name2, "%d", column_index);
+                graph_create_edge(node_name, node_name2);
             }
-            else
-            {
-                node2_name[strlen(node2_name)] = last_char;
-            }
-            break;
+
+            last_char = fgetc(stream_s);
         }
 
-        if (edge_end && strlen(node_name) != 0 && strlen(node2_name) != 0)
-        {
-            graph_create_edge(node_name, node2_name);
-            int node_name_len = (int)strlen(node_name);
-            for (int i = 0; i < node_name_len; i++)
-            {
-                node_name[i] = '\0';
-            }
-            int node2_name_len = (int)strlen(node2_name);
-            for (int i = 0; i < node2_name_len; i++)
-            {
-                node2_name[i] = '\0';
-            }
-            edge_start = false;
-            edge_split = false;
-            edge_end = false;
-        }
-
-        columns++;
+        last_char = fgetc(stream_s);
+        lines++;
     }
 }
 
@@ -249,8 +99,6 @@ void parse_data(FILE *stream)
     graph_init();
 
     stream_s = stream;
-
-    parse_node_data();
 
     parse_edge_data();
 }
